12902b4e67dcb6c63f239d356859161820293220
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-10-04 10:32:32 +0530

    completed interview card

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import dayjs from 'dayjs';
 import Image from 'next/image';
-import { getRandomInterviewCover } from '@/public/utils';
+import { getRandomInterviewCover } from '@/lib/utils';
+import { Button } from './ui/button';
+import Link from 'next/link';
+import DisplayTechIcons from './DisplayTechIcons';
 
 const InterviewCard = ({interviewId , userId , role , type , techstack, createdAt}: InterviewCardProps) => {
   const feedback = null as Feedback | null;
@@ -16,6 +19,28 @@ const InterviewCard = ({interviewId , userId , role , type , techstack, createdA
             <p className='badge-text'>{normalizedType}</p>
           </div>
           <Image src={getRandomInterviewCover()} alt='interview-cover' width={90} height={90} className='rounded-full object-fit size-[90px]'/>
+          <h3 className='mt-5 capitalize'>{role} Interview</h3>
+          <div className='flex flex-row gap-5 mt-3'>
+            <div className='flex flex-row gap-2'>
+              <Image src='/calendar.svg' alt='calendar-icon' width={22} height={22}/>
+              <p>{formattedDate}</p>
+            </div>
+            <div className='flex flex-row gap-2 items-center'>
+              <Image src='/star.svg' alt='star-icon' width={22} height={22}/>
+              <p>{feedback?.totalScore || '---'}/100</p>
+            </div>
+          </div>
+          <p className='line-clamp-2 mt-5'>
+            {feedback?.finalAssessment || 'No feedback available yet. Please complete the interview to receive feedback.'}
+          </p>
+        </div>
+        <div className='flex flex-row justify-between'>
+          <DisplayTechIcons techStack={techstack}/>
+          <Button className='btn-primary'>
+            <Link href={feedback ? `/interview/${interviewId}/feedback` : `/interview/${interviewId}`}>
+            {feedback ? 'View Feedback' : 'Take Interview'}
+            </Link>
+          </Button>
         </div>
       </div>
     </div>
