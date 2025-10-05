8878a839f5e8ba7d7368c4af3b40ca3022ee511f
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-10-05 10:24:09 +0530

    firebase setup

---
+++ firebase/client.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp , getApp , getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIrZ8JX1fPkzDeeQCALQRH5ZycHWrkXIY",
+  authDomain: "prep-ai-bc833.firebaseapp.com",
+  projectId: "prep-ai-bc833",
+  storageBucket: "prep-ai-bc833.firebasestorage.app",
+  messagingSenderId: "674745696267",
+  appId: "1:674745696267:web:f6eea8d93622e58043470e",
+  measurementId: "G-PV9HH7LR49"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
